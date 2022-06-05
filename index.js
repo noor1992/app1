98d2fcb55f181d5231961fc7bfa0b6b04cde0922
noor1992 <noor.muradi@protonmail.com>
2022-06-05 15:09:27 +0530

    index updated (#9)
    * Dev (#5)
    
    * Update index.js
    
    Port changed to 3004
    
    * Update index.js
    
    port changed to 3005
    
    Co-authored-by: noorrozgar <104819467+noorrozgar@users.noreply.github.com>
    
    * index updated
    
    Co-authored-by: noorrozgar <104819467+noorrozgar@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-res.send('<h1><center>Nodejs app workig !!!<center></h1>');
+res.send('<h1><center>Nodejs app workig !<center></h1>');
 });
+
 
 
 app.listen(3008, () => {
   console.log('App listening on port 3008!');
+
 
 });
